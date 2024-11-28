8690e3d2c868ef2493ebf2379cddb98cbc97f20d
Sadre_321 <haydaremre31@gmail.com>
2024-11-28 14:08:34 +0300

    Layout olusturuldu ve slider eklendi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Footer from './Components/Layouts/Footer/Footer'
+import Header from './Components/Layouts/Header/Header'
+import Policy from './Components/Layouts/Policy/Policy'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <h1>Hello</h1>
+        <Policy/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
